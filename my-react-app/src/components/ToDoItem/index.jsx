3af4f8b6190b97024db1667e54479911a0ff4e88
RoryPyman <rory.pyman@gmx.com>
2024-09-17 11:41:18 +0100

    extra css

--- my-react-app/src/components/ToDoItem/index.jsx
+++ my-react-app/src/components/ToDoItem/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.css'
 function TodoItem({ task, deleteTask, toggleCompleted}) {
     function handleChange() {
     toggleCompleted(task.id);
@@ -7,12 +8,14 @@ function TodoItem({ task, deleteTask, toggleCompleted}) {
     return (
         <div className="todo-item">
             <p>{task.text}</p>
-            <input 
-                type="checkbox"
-                checked={task.completed}
-                onChange={handleChange}
-            />
             <p className='importance'>Importance: {task.importance}</p>
+            <div className='checkboxlabel'> Completed 
+                <input 
+                    type="checkbox"
+                    checked={task.completed}
+                    onChange={handleChange}
+                />
+            </div>
             <button onClick={() => deleteTask(task.id)}> Delete Task </button>
         </div>
     );
