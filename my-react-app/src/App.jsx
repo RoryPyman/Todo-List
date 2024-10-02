104d570a875d650ea55b3e7352bb9f51295aa571
RoryPyman <rory.pyman@gmx.com>
2024-10-02 18:31:10 +0100

    Added logo and title using helmets

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,21 +4,51 @@ import Home from './components/Home/index'
 import Register from './components/Register'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
+import { Helmet, HelmetProvider } from 'react-helmet-async';  // Optional for setting titles
 
 function App() {
 
   return (
-    
-    <div className='App'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element= {<Home />} />
-          <Route path='/list' element= {<ToDoList />} />
-          <Route path='/login' element= {<Login />} />
-          <Route path='/register' element= {<Register />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <HelmetProvider>
+      <div className='App'>
+        <BrowserRouter>
+          <Routes>
+          <Route 
+              path='/' 
+              element={<>
+                  <Helmet>
+                    <title>Home - Todo App</title>
+                  </Helmet>
+                  <Home />
+                </>} />
+            <Route 
+              path='/list' 
+              element={<>
+                  <Helmet>
+                    <title>To-Do List - Todo App</title>
+                  </Helmet>
+                  <ToDoList />
+                </>} />
+            <Route 
+              path='/login' 
+              element={<>
+                  <Helmet>
+                    <title>Login - Todo App</title>
+                  </Helmet>
+                  <Login />
+                </>} />
+            <Route 
+              path='/register' 
+              element={<>
+                  <Helmet>
+                    <title>Register - Todo App</title>
+                  </Helmet>
+                  <Register />
+                </>} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </HelmetProvider>
   )
 }
 
